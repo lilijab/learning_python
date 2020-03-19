b00476ec6276644c3349143773ebedec117d0d0f
lilijab <lilija.buidovaite@gmail.com>
2020-03-19 08:38:22 +0000

    ascii art example

---
+++ ascii_art.py
@@ -0,0 +1,19 @@
+from pyfiglet import figlet_format as f
+from termcolor import colored as c
+
+
+def ascii_print():
+
+    message = input('What message do you want to print? ')
+
+    while True:
+        try:
+            colour = input('What colour? ')
+            answer = c(f(message), colour)
+            break
+        except KeyError:
+            print('Colour is not valid. Please choose from red, green, yellow, blue, magenta, cyan, white')
+    return print(answer)
+
+
+ascii_print()
